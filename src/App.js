61ed4add894014a6823e94253a777106b2b61510
Chris Drew <github@rotarydial.com>
2024-04-11 16:56:59 -0400

    chore: baseline app files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Generate randomized <a href="https://theworld.com/~reinhold/diceware.html" target="_blank">Diceware</a> passphrases (as made famous in <a href="https://xkcd.com/936/">xkcd #936</a>).
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <Wrapper />
     </div>
   );
 }

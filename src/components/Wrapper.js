a380bddacfce4a484aeeecf27b297f01a20eb770
Chris Drew <github@rotarydial.com>
2024-04-12 01:15:45 -0400

    feat: sliders to control phrases, words

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Phrases from './Phrases';
+import { Slider, Text, Box } from '@mantine/core';
 
 export default function Wrapper( props ) {
+  const [numWords, setNumWords] = useState(4);
+  const [endNumWords, setEndNumWords] = useState(4);
+  
+  const [numPhrases, setNumPhrases] = useState(3);
+  const [endNumPhrases, setEndNumPhrases] = useState(3);
 
   return (
     <>
-      <Phrases wordCount={ 2 } phraseCount = { 5 } />
-      <Phrases wordCount={ 1 } phraseCount = { 2 } />
-      <Phrases wordCount={ 2 } phraseCount = { 1 } />
-      <Phrases wordCount={ 10 } phraseCount = { 20 } />
+      <Box maw={200} mx="auto">
+        <Text mt="md" size="sm">
+          phrases: { endNumPhrases }
+        </Text>
+        <Slider 
+          value={numPhrases} 
+          min={1}
+          max={30}
+          onChange={setNumPhrases} 
+          onChangeEnd={setEndNumPhrases} 
+        />
+      </Box>
+
+      <Box maw={200} mx="auto">
+        <Text mt="md" size="sm">
+          words per phrase: { endNumWords }
+        </Text>
+        <Slider 
+          value={numWords} 
+          min={2}
+          max={20}
+          onChange={setNumWords} 
+          onChangeEnd={setEndNumWords} 
+        />
+      </Box>
+
+      <div className='phraseWrapper'>
+        <Phrases wordCount={ endNumWords } phraseCount = { endNumPhrases } />
+      </div>
     </>
   )
 }

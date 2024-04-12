7734cf271df7cf2978506823b0d8f352faa79994
Chris Drew <github@rotarydial.com>
2024-04-12 02:59:03 -0400

    chore: copyright, links

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
       </div>
 
       <footer className="App-footer">
-        <><sup>*</sup> As made famous in <a href="https://xkcd.com/936/"  target="_blank" rel="noreferrer">xkcd #936</a></>
+        <><sub><sup>*</sup>As made famous in <a href="https://xkcd.com/936/"  target="_blank" rel="noreferrer">xkcd #936</a></sub></>
+        <div>© 2024, Christopher Drew / rooftree.io. The code for this site is open source, and can be found <a href="https://github.com/rotarydialer/diceware.org" target="_blank" rel="noreferrer">on GitHub here</a></div>
       </footer>
     </div>
   );

2ee42bd6629d3cfe948278a781dd70d035051875
Chris Drew <github@rotarydial.com>
2024-04-12 01:14:58 -0400

    chore: wire up Mantine

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );

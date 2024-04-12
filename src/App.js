9511246e8c069610d8c9d3cef07144f0d3b20fd3
Chris Drew <github@rotarydial.com>
2024-04-12 01:53:00 -0400

    feat: app footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
 import Wrapper from './components/Wrapper';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='siteTitle'>
-          Generate randomized <a href="https://theworld.com/~reinhold/diceware.html" target="_blank" rel="noreferrer">Diceware</a> passphrases.
-        </div>
-        <div className='siteTagline'>
-          <i>(as made famous in <a href="https://xkcd.com/936/"  target="_blank" rel="noreferrer">xkcd #936</a>)</i>
-        </div>
+        <h1 className='siteTitle'>
+          Generate randomized <a href="https://theworld.com/~reinhold/diceware.html" target="_blank" rel="noreferrer">Diceware</a> passphrases.<sup>*</sup>
+        </h1>
       </header>
 
-      <Wrapper />
+      <div className='content'>
+        <Wrapper />
+      </div>
+
+      <footer className="App-footer">
+        <><sup>*</sup> As made famous in <a href="https://xkcd.com/936/"  target="_blank" rel="noreferrer">xkcd #936</a></>
+      </footer>
     </div>
   );
 }
